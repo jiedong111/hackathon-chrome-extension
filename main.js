8e04353176ee3acee0dc437f13dd19a655d53fea
larkinaj <larkin.aj@gmail.com>
2023-01-21 20:23:05 -0500

    refactored code to more optimally highlight text

--- main.js
+++ main.js
@@ -4,19 +4,19 @@
 //Use 
 
 document.addEventListener('keydown', (e) => {
-    if (e.code === 'ArrowLeft') { //use left arrow for now
-      // chrome.storage.local.get(function(result) {
-      //   console.log('storage: ', result)
-      // })
+    if (e.code === 'Backquote') { //use left arrow for now
+
       const currSelection = window.getSelection();
       let childNodesVar = currSelection.anchorNode.parentNode.childNodes
       const parentNodeVar = currSelection.anchorNode.parentNode;
       const newText = currSelection.toString()
 
       console.log('currSelection: ', currSelection);
-      console.log('anchor node: ', currSelection.anchorNode);
-      console.log('focus node', currSelection.focusNode)
-      console.log('currSelection parent node: ', currSelection.anchorNode.parentNode)
+
+
+
+
+      
 
       //lines 21 - 35 adds notes to popup.html/local storage---------------------
       let websiteURL = window.location.hostname
@@ -35,87 +35,94 @@ document.addEventListener('keydown', (e) => {
       })
       //------------------------------------------------------------------------
 
-      let numOfChildNodes = new Map();
-      let count = false
-      for (let i = 0; i < childNodesVar.length; i++) {
-        if (childNodesVar[i].data === currSelection.anchorNode.data || (childNodesVar[i].data === undefined && childNodesVar.innerHTML === currSelection.anchorNode.data)) {
-          count = true
-        } 
-        if (count === true) {
-          // numOfChildNodes.push(childNodesVar[i]);
-          numOfChildNodes.set(childNodesVar[i], childNodesVar[i].parentNode);
-        }
-        if (childNodesVar[i].data === currSelection.focusNode.data|| (childNodesVar[i].data === undefined && childNodesVar[i].innerHTML === currSelection.focusNode.data)) {
-          count = false;
-        }
-      }
-      console.log('numofNOde array: ', numOfChildNodes)
+        let highlightNode = document.createElement('span');
+        highlightNode.classList.add('highlight');
+        const currSelectionRange = currSelection.getRangeAt(0);
+        let workSelection = currSelectionRange.extractContents();
+        highlightNode.appendChild(workSelection);
+        currSelectionRange.insertNode(highlightNode);
 
-      let i = 0;
-      if (numOfChildNodes.size > 1) {
-        for (let [key, value] of numOfChildNodes) {
-          console.log(i);
-          console.log('numofNOde at index i: ', numOfChildNodes[i])
-          let testSib = key.nextSibling;
-          if (i === 0) {
-            let firstHalfAnchor = document.createElement('span');
-            let secondHalfAnchor = document.createElement('span');
-            let firstHalfAnchorText = currSelection.anchorNode.data.slice(0, currSelection.anchorOffset);
-            let secondHalfAnchorText = currSelection.anchorNode.data.slice(currSelection.anchorOffset, currSelection.anchorNode.data.length)
-            firstHalfAnchor.innerHTML = firstHalfAnchorText
-            secondHalfAnchor.innerHTML = secondHalfAnchorText
-            secondHalfAnchor.classList.add('highlight')
-            value.removeChild(key)
-            currSelection.anchorNode.insertBefore(firstHalfAnchor, testSib)
-            currSelection.anchorNode.insertBefore(secondHalfAnchor, testSib)
-          }
-          else if (i === numOfChildNodes.size - 1) {
-            let secondHalfFocus = document.createElement('span');
-            let firstHalfFocus = document.createElement('span');
-            let secondHalfFocusText = currSelection.focusNode.data.slice(currSelection.focusOffset, currSelection.focusNode.data.length);
-            const firstHalfFocusText = currSelection.focusNode.data.slice(0, currSelection.focusOffset)
-            secondHalfFocus.innerHTML = secondHalfFocusText
-            firstHalfFocus.innerHTML = firstHalfFocusText
-            firstHalfFocus.classList.add('highlight')
-            value.removeChild(key)
-            currSelection.anchorNode.insertBefore(firstHalfFocus, testSib)
-            currSelection.anchorNode.insertBefore(secondHalfFocus, testSib)
-          }
-          else {
-            let newMiddle = document.createElement('span');
-            let newMiddleText = key.data
-            if (key.data === undefined) newMiddleText = key.innerHTML
-            newMiddle.innerText = newMiddleText
-            newMiddle.classList.add('highlight')
-            console.log(value);
-            value.removeChild(key)
-            // currSelection.anchorNode.parentNode.insertBefore(newMiddle, testSib)
-            value.insertBefore(newMiddle, testSib);
-          }
-          i++;
-        }
-      } 
-      else {
-        let htmlElement = document.createElement('span')
-        const nextSibling = currSelection.anchorNode.nextSibling;
-        const firstHalfText = currSelection.anchorNode.data.slice(0, currSelection.anchorOffset);
-        const secondHalfText = currSelection.anchorNode.data.slice(currSelection.focusOffset, currSelection.anchorNode.data.length);
-        const firstHalf = document.createElement('span')
-        const secondHalf = document.createElement('span')
-        firstHalf.innerHTML = firstHalfText
-        secondHalf.innerHTML = secondHalfText
-        htmlElement.innerHTML = newText
-        htmlElement.classList.add('highlight')
-        currSelection.anchorNode.parentNode.removeChild(currSelection.anchorNode)
-        currSelection.anchorNode.insertBefore(firstHalf, nextSibling)
-        currSelection.anchorNode.insertBefore(htmlElement, nextSibling)
-        currSelection.anchorNode.insertBefore(secondHalf, nextSibling)
-      }
+
+        console.log('currSelectionRange', currSelectionRange)
+        console.log('workSelection', workSelection)
+        console.log('highlightNode', highlightNode)
+
+      // let numOfChildNodes = new Map();
+      // let count = false
+      // for (let i = 0; i < childNodesVar.length; i++) {
+      //   if (childNodesVar[i].data === currSelection.anchorNode.data || (childNodesVar[i].data === undefined && childNodesVar.innerHTML === currSelection.anchorNode.data)) {
+      //     count = true
+      //   } 
+      //   if (count === true) {
+      //     // numOfChildNodes.push(childNodesVar[i]);
+      //     numOfChildNodes.set(childNodesVar[i], childNodesVar[i].parentNode);
+      //   }
+      //   if (childNodesVar[i].data === currSelection.focusNode.data|| (childNodesVar[i].data === undefined && childNodesVar[i].innerHTML === currSelection.focusNode.data)) {
+      //     count = false;
+      //   }
+      // }
+      // console.log('numofNOde array: ', numOfChildNodes)
+
+      // let i = 0;
+      // if (numOfChildNodes.size > 1) {
+      //   for (let [key, value] of numOfChildNodes) {
+      //     console.log(i);
+      //     console.log('numofNOde at index i: ', numOfChildNodes[i])
+      //     let testSib = key.nextSibling;
+      //     if (i === 0) {
+      //       let firstHalfAnchor = document.createElement('span');
+      //       let secondHalfAnchor = document.createElement('span');
+      //       let firstHalfAnchorText = currSelection.anchorNode.data.slice(0, currSelection.anchorOffset);
+      //       let secondHalfAnchorText = currSelection.anchorNode.data.slice(currSelection.anchorOffset, currSelection.anchorNode.data.length)
+      //       firstHalfAnchor.innerHTML = firstHalfAnchorText
+      //       secondHalfAnchor.innerHTML = secondHalfAnchorText
+      //       secondHalfAnchor.classList.add('highlight')
+      //       value.removeChild(key)
+      //       currSelection.anchorNode.insertBefore(firstHalfAnchor, testSib)
+      //       currSelection.anchorNode.insertBefore(secondHalfAnchor, testSib)
+      //     }
+      //     else if (i === numOfChildNodes.size - 1) {
+      //       let secondHalfFocus = document.createElement('span');
+      //       let firstHalfFocus = document.createElement('span');
+      //       let secondHalfFocusText = currSelection.focusNode.data.slice(currSelection.focusOffset, currSelection.focusNode.data.length);
+      //       const firstHalfFocusText = currSelection.focusNode.data.slice(0, currSelection.focusOffset)
+      //       secondHalfFocus.innerHTML = secondHalfFocusText
+      //       firstHalfFocus.innerHTML = firstHalfFocusText
+      //       firstHalfFocus.classList.add('highlight')
+      //       value.removeChild(key)
+      //       currSelection.anchorNode.insertBefore(firstHalfFocus, testSib)
+      //       currSelection.anchorNode.insertBefore(secondHalfFocus, testSib)
+      //     }
+      //     else {
+      //       let newMiddle = document.createElement('span');
+      //       let newMiddleText = key.data
+      //       if (key.data === undefined) newMiddleText = key.innerHTML
+      //       newMiddle.innerText = newMiddleText
+      //       newMiddle.classList.add('highlight')
+      //       console.log(value);
+      //       value.removeChild(key)
+      //       // currSelection.anchorNode.parentNode.insertBefore(newMiddle, testSib)
+      //       value.insertBefore(newMiddle, testSib);
+      //     }
+      //     i++;
+      //   }
+      // } 
+      // else {
+      //   let htmlElement = document.createElement('span')
+      //   const nextSibling = currSelection.anchorNode.nextSibling;
+      //   const firstHalfText = currSelection.anchorNode.data.slice(0, currSelection.anchorOffset);
+      //   const secondHalfText = currSelection.anchorNode.data.slice(currSelection.focusOffset, currSelection.anchorNode.data.length);
+      //   const firstHalf = document.createElement('span')
+      //   const secondHalf = document.createElement('span')
+      //   firstHalf.innerHTML = firstHalfText
+      //   secondHalf.innerHTML = secondHalfText
+      //   htmlElement.innerHTML = newText
+      //   htmlElement.classList.add('highlight')
+      //   currSelection.anchorNode.parentNode.removeChild(currSelection.anchorNode)
+      //   currSelection.anchorNode.insertBefore(firstHalf, nextSibling)
+      //   currSelection.anchorNode.insertBefore(htmlElement, nextSibling)
+      //   currSelection.anchorNode.insertBefore(secondHalf, nextSibling)
+      // }
     }
 })
 
-// deleteButton.addEventListener('click', (e) => {
-//   chrome.storage.local.clear()
-//   const main = document.querySelector('.popup-main')
-//   main.innerHTML = '';
-// })
--- popup.js
+++ popup.js
@@ -28,54 +28,6 @@ deleteButton.addEventListener('click', (e) => {
   main.innerHTML = '';
 })
 
-// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-
 //     // chrome.storage.local.get(function(result) {
 //     //   console.log(result)
 //     // })
-
-//       let highlightedText = request.greeting;
-//       let websiteURL = sender.origin.slice(8)
-
-//       const main = document.querySelector('main')
-//       const newDiv = document.createElement('div')
-
-
-//       newDiv.classList.add('website-divs')
-
-//       let storageArr;
-//       chrome.storage.local.get(function(data) {
-//         console.log('get request: ', data[websiteURL])
-//         if (data[websiteURL] === undefined) {
-//           storageArr = [];
-//         } 
-//         else if (data[websiteURL] !== undefined) {
-//           storageArr = data[websiteURL]
-//         }
-        
-//         storageArr.push(highlightedText)
-//         console.log('storageArr: ', storageArr)
-  
-//         let storageObj = {}
-//         storageObj[websiteURL] = storageArr;
-  
-//         console.log(storageObj)
-//         chrome.storage.local.set(storageObj);
-
-//         for (const key in data) {
-//           main.innerHTML = '';
-//           const newTitle = document.createElement('h3');
-//           const newerUL = document.createElement('ul');
-//           newTitle.innerText = 'Notes from ' + key + ':'
-//           main.appendChild(newDiv)
-//           newDiv.appendChild(newTitle)
-//           newDiv.appendChild(newerUL)
-//           for (let i = 0; i < data[key].length; i++) {
-//             const newerLI = document.createElement('li')
-//             newerLI.innerText = data[key][i]
-//             newerUL.appendChild(newerLI)
-//           }
-//         }
-//       })
-//       sendResponse({farewell: 'goodbye'})
-// })
